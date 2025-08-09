f5d9614eb8fb9bd2b0d9f5c6603c79cdb1e8f083
alstongit <alstondsouza57@gmail.com>
2025-08-09 19:46:33 +0530

    viteconfig

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -5,8 +5,8 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || "/",
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/AlstonPortfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
